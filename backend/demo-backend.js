e144bb3c476f45ed9a5d6bd02ad28edc620a4cfe
Tania <Khokiavko@gmail.com>
2025-04-15 18:46:40 +0300

    fake api

--- backend/demo-backend.js
+++ backend/demo-backend.js
@@ -4,58 +4,102 @@ const port = 3000
 
 app.use(express.json())
 
-let words = ['mouse', 'school', 'task', 'exam', 'theory']
-let wordIndex = 0;
-let round = 0;
-let score = 0
 
-app.get('/', (req, res) => {
-    res.send(`Game Alias work ${port}`)
+let model = {
+    roomId: 12,
+    teams: [
+        {
+            name: 'team1',
+            score: 5,
+        },
+
+    ],
+    activeTeamIndex: null,
+    round: 1,
+    guessed: 0,
+    skip: 0,
+    winner: null,
+}
+
+app.post('/games/new-room', (req, res) => {  //створюємо нову пусту кімната для гри з її номером
+    let newRoomId = model.roomId++;
+    let newRoom = {
+        newRoomId,
+        teams: [],
+        activeTeamIndex: null,
+        round: 1,
+        guessed: 0,
+        skip: 0,
+        winner: null,
+    };
+    // model.push(newRoom)
+
+    res.json(newRoom)
 })
 
-app.post('/new-game', (req, res) => {
-    round = 1;
-    score = 0;
-    wordIndex = 0;
-    res.json({massage: 'New game'})
-})
-
-app.post('/correct', (req, res) => {
-    score ++
-    wordIndex ++;
-    res.redirect('/round')
-})
-
-app.get('/next-word', (req, res) => {
-    score ++
-    wordIndex ++
-    res.redirect('/round')
-})
-
-app.get('/round', (req, res) => {
-    if (round > 0 && wordIndex < words.length){
-        res.send(`
-        <!DOCTYPE html>
-<html lang="ea">
-<head>
-    <meta charset="UTF-8">
-    <title>Alias</title>
-</head>
-<body>
-<h1 id="word"><h1 id="word">${words[wordIndex]}</h1>
-<form action="/correct" method="POST" style="display:inline;">
-    <button type="submit">Вгадав</button>
-</form>
-
-<form action="/next-word" method="GET" style="display:inline;">
-    <button type="submit">Не вгадав</button>
-</form>
-</body>
-</html>
-
-        `)
+app.get('/games/:roomId', (req, res) => {       //отримаємо гру по номеру
+    const roomId = parseInt(req.params.roomId)
+    if(model.roomId === roomId){
+        res.json(model)
+    }else{
+        return res.status(404).json({ error: 'Room not found' })
     }
 })
+
+app.post('/games/:roomId/teams/:teamName', (req, res) => { // додаємо нову створену команду в кімнату
+    const roomId = parseInt(req.params.roomId)
+    const teamName = req.params.teamName
+    if(model.roomId === roomId){
+        model.teams.name = teamName
+    }
+    res.json(model)
+})
+
+app.delete('/games/:roomId/teams/:teamName', (req, res) => {
+    const teamName = req.params.teamName
+    const roomId = parseInt(req.params.roomId)
+    if(model.teams.name === teamName && model.roomId === roomId){
+        delete model
+    }
+})
+
+app.put('/games/:roomId/teams/:teamName/score/:count', (req, res) => { // встановлюємо рахунок після раунду
+    const roomId = parseInt(req.params.roomId)
+    const teamName = req.params.teamName
+    const score = parseInt(req.params.count)
+    if(model.teams.name === teamName && model.roomId === roomId){
+        model.teams.score = score
+    }
+    res.json(model)
+})
+
+app.put('/games/:roomId/round/:number', (req, res) => {     //встановлюємо  раунду
+    const roomId = parseInt(req.params.roomId)
+    const round = parseInt(req.params.number)
+    if(model.roomId === roomId){
+        model.round = round
+    }
+    res.json(model)
+})
+
+app.put('/games/:roomId/winner/:teamIdx', (req, res) => {
+    const roomId = parseInt(req.params.roomId)
+    const winner = parseInt(req.params.teamIdx)
+    if(model.roomId === roomId){
+        model.winner = winner
+    }
+    res.json(model)
+})
+
+app.put('/games/:roomId/active-team-index/:activeTeamIndex', (req, res) => {
+    const roomId = parseInt(req.params.roomId)
+    const activeTeamIndex = parseInt(req.params.activeTeamIndex)
+    if(model.roomId === roomId){
+        model.activeTeamIndex = activeTeamIndex
+    }
+    res.json(model)
+})
+
 
 app.listen(port, () => {
     console.log((`Alias serwer work on http://localhost:${port} `))
